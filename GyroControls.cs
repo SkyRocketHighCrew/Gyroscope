db529a4d5c6d5952e92440784f4956ce72b8e2ed
Seokin Yoon <56791319+ynskn@users.noreply.github.com>
2019-12-08 13:30:43 +0900

    Add files via upload

---
+++ GyroControls.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroControls : MonoBehaviour
+{
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+
+    private GameObject cameraContainer;
+    private Quaternion rot;
+    private void Start()
+    {
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+
+        gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro()
+    {
+        if(SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+            rot = new Quaternion(0, 0, 1, 0);
+
+
+            return true;
+        }
+        //the only way to test gyroscope is to test it out on your phone ㅠㅠ
+
+        return false;
+    }
+
+    private void Update()
+    {
+       if(gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rot;
+            
+        }
+    }
+}
